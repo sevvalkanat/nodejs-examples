f05fdc548ab2f28a19ac32b0a640e8b522f23af0
sevvalkanat <84670473+sevvalkanat@users.noreply.github.com>
2022-03-18 13:53:29 +0300

    Delete circle.js

--- circle.js
+++
@@ -1,8 +0,0 @@
-function circleArea (R) {
-    console.log(Math.PI*(R*R));
-}
-function circleCircumference (R){
-    console.log(Math.PI*(2 * R))
-}
-module.exports = {circleArea,circleCircumference};
- 
